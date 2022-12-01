a377bf917e2ec13db43a31ef1847d424d7a92432
Tri Buono Asto Nugroho <tribuonoasto@gmail.com>
2022-12-01 13:26:25 +0700

    remove env

--- server/app.js
+++ server/app.js
@@ -1,6 +1,3 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
 const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 const router = require("./routes");
